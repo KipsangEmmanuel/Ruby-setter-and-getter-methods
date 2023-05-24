ba208ce9505afe488b9936160c6440a2c7bbe348
Emmanuel Kipsang <emmqnuelkipsqng@gmail.com>
2023-05-24 15:30:20 -0700

    add person's getter and setter methods

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,28 @@
 class Person
 
+    attr_accessor :job
+    
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
+
+    # def job=(job)
+    #     @job = job
+    # end
+
+    # def job
+    #     @job
+    # end
 end
